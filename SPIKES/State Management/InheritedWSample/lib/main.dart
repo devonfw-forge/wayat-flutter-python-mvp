5d3f4bfabb0b6041b9c5b2949c52ec3ff42aaf2c
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2022-07-15 08:14:52 +0200

    Change names in InheritedW

--- SPIKES/State Management/InheritedWSample/lib/main.dart
+++ SPIKES/State Management/InheritedWSample/lib/main.dart
@@ -1,37 +1,55 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'models/my_colors.dart';
+=======
+import 'models/SampleColors.dart';
+>>>>>>> Stashed changes
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SampleApp extends StatelessWidget {
+  const SampleApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< Updated upstream
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: MyPage());
+=======
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SamplePage()
+    );
+>>>>>>> Stashed changes
   }
 }
 
-class MyPage extends StatefulWidget {
-  MyPage({Key? key}) : super(key: key);
+class SamplePage extends StatefulWidget {
+  SamplePage({Key? key}) : super(key: key);
 
   @override
-  State<MyPage> createState() => _MyPageState();
+  State<SamplePage> createState() => _SamplePageState();
 }
 
-class _MyPageState extends State<MyPage> {
+class _SamplePageState extends State<SamplePage> {
   Color color1 = Colors.redAccent;
   Color color2 = Colors.teal;
+  
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return MyColorsW(
+=======
+    return ColorsW(
+>>>>>>> Stashed changes
       color1: color1,
       color2: color2,
       child: Scaffold(
@@ -56,12 +74,23 @@ class _MyPageState extends State<MyPage> {
 class Rectangle1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     final myColorsW = context.dependOnInheritedWidgetOfExactType<MyColorsW>();
+=======
+
+    final colorsW = context.dependOnInheritedWidgetOfExactType<ColorsW>();
+>>>>>>> Stashed changes
 
     return Container(
       width: 70,
       height: 70,
+<<<<<<< Updated upstream
       decoration: BoxDecoration(color: myColorsW?.color1),
+=======
+      decoration: BoxDecoration(
+        color: colorsW?.color1
+      ),
+>>>>>>> Stashed changes
     );
   }
 }
@@ -69,12 +98,22 @@ class Rectangle1 extends StatelessWidget {
 class Rectangle2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     final myColorsW = context.dependOnInheritedWidgetOfExactType<MyColorsW>();
+=======
+    final colorsW = context.dependOnInheritedWidgetOfExactType<ColorsW>();
+>>>>>>> Stashed changes
 
     return Container(
       width: 70,
       height: 70,
+<<<<<<< Updated upstream
       decoration: BoxDecoration(color: myColorsW?.color2),
+=======
+      decoration: BoxDecoration(
+        color: colorsW?.color2
+      ),
+>>>>>>> Stashed changes
     );
   }
 }
