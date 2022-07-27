e29259dcb3e6d94d01876e7cd32c4de09ec835b6
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-07-27 13:49:44 +0200

    First steps

--- wayat/frontend/lib/services/contact/contact_service.dart
+++ wayat/frontend/lib/services/contact/contact_service.dart
@@ -2,5 +2,5 @@ import 'package:wayat/domain/contact/contact.dart';
 import 'package:wayat/services/service.dart';
 
 abstract class ContactService extends Service {
-  List<Contact> getAll();
+  Future<List<Contact>> getAll();
 }
--- wayat/frontend/lib/services/contact/contact_service_impl.dart
+++ wayat/frontend/lib/services/contact/contact_service_impl.dart
@@ -1,10 +1,22 @@
 import 'package:wayat/domain/contact/contact.dart';
 import 'package:wayat/services/contact/contact_service.dart';
-import 'package:wayat/services/contact/mock/contacts_mock.dart';
+import 'package:flutter_contacts/flutter_contacts.dart' as addressBookContact;
 
 class ContactServiceImpl extends ContactService {
   @override
-  List<Contact> getAll() {
-    return ContactsMock.contacts;
+  Future<List<Contact>> getAll() async {
+    if (await addressBookContact.FlutterContacts.requestPermission()) {
+      var contacts = await addressBookContact.FlutterContacts.getContacts(
+              withProperties: true, withPhoto: true)
+          .then((contact) {
+        return Contact(
+            available: true,
+            displayName: contact.displayName,
+            username: "active",
+            email: "user@mail.com",
+            imageUrl: "url");
+      });
+      return contacts;
+    }
   }
 }
