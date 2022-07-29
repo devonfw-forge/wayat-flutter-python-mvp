1538b7c514b4f3815b5cc15b14ef23264480f2d9
airherna <airam.hernandez-rocha@capgemini.com>
2022-07-29 14:06:51 +0200

    Revert "Add Random() latitude, longtitude function"
    This reverts commit b47f5149bcdd1579e830b0ea302d5856a33b9786.

--- wayat/frontend/lib/services/location/location_service_impl.dart
+++ wayat/frontend/lib/services/location/location_service_impl.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:wayat/domain/location/location.dart';
 import 'package:wayat/services/location/location_service.dart';
 import 'mock/location-mock.dart';
@@ -8,15 +6,5 @@ class LocationServiceImpl extends LocationService {
   @override
   List<Location> getAll() {
     return LocationMock.getLocation();
-  }
-
-  void RandomCoordinates() {
-    final random = Random();
-    double nextDouble(num min, num max) =>
-        min + random.nextDouble() * (max - min);
-
-    double randomLat = nextDouble(-90, 90);
-    double randomLng = nextDouble(-180, 180);
-    print('$randomLat, $randomLng');
   }
 }
--- wayat/frontend/lib/services/location/mock/location-mock.dart
+++ wayat/frontend/lib/services/location/mock/location-mock.dart
@@ -2,26 +2,16 @@ import 'package:wayat/domain/location/location.dart';
 
 class LocationMock {
   static final List<Location> coordinates = [
-    //Dnipro
-    Location(latitude: 48.450001, longitude: 34.983334),
-    //Barcelona
-    Location(latitude: 41.390205, longitude: 2.154007),
-    //Larnaka
-    Location(latitude: 34.923096, longitude: 33.634045),
-    //Villazon
-    Location(latitude: -22.090887, longitude: -65.596832),
-    //Shanghai
-    Location(latitude: 31.224361, longitude: 121.469170),
-    //Tallin
-    Location(latitude: 59.436962, longitude: 24.753574),
-    //Lyon
-    Location(latitude: 45.763420, longitude: 4.834277),
-    //Athens
-    Location(latitude: 37.983810, longitude: 23.727539),
-    //Dublin
-    Location(latitude: 53.350140, longitude: -6.266155),
-    //Oslo
-    Location(latitude: 59.911491, longitude: 10.757933),
+    Location(city: 'Dnipro', latitude: 48.450001, longitude: 34.983334),
+    Location(city: 'Barcelona', latitude: 41.390205, longitude: 2.154007),
+    Location(city: 'Larnaka', latitude: 34.923096, longitude: 33.634045),
+    Location(city: 'Villazon', latitude: -22.090887, longitude: -65.596832),
+    Location(city: 'Shanghai', latitude: 31.224361, longitude: 121.469170),
+    Location(city: 'Tallin', latitude: 59.436962, longitude: 24.753574),
+    Location(city: 'Lyon', latitude: 45.763420, longitude: 4.834277),
+    Location(city: 'Athens', latitude: 37.983810, longitude: 23.727539),
+    Location(city: 'Dublin', latitude: 53.350140, longitude: -6.266155),
+    Location(city: 'Oslo', latitude: 59.911491, longitude: 10.757933),
   ];
 
   static List<Location> getLocation() {
