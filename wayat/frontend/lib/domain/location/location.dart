5bc2c2211d4cf163e66b444713305dd40733b712
airherna <airam.hernandez-rocha@capgemini.com>
2022-07-29 14:06:57 +0200

    Revert "Add Location entity and Location abstract service class"
    This reverts commit 49a9866f3b81d8d171f39d972c29790187cc64eb.

--- wayat/frontend/lib/domain/location/location.dart
+++
@@ -1,8 +0,0 @@
-class Location {
-  final String city;
-  final double latitude;
-  final double longitude;
-
-  Location(
-      {required this.city, required this.latitude, required this.longitude});
-}
--- wayat/frontend/lib/services/location/location_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wayat/domain/location/location.dart';
-import 'package:wayat/services/service.dart';
-
-abstract class LocationService extends Service {
-  List<Location> getAll();
-}
