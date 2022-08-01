425a4a29fc23153aa987a28dbea5b2b87ad2f0ed
Serhii Bets <108925503+serhiibets@users.noreply.github.com>
2022-08-01 13:29:25 +0200

    Convert meters offset to lat and lng

--- wayat/frontend/lib/services/location/location_service_impl.dart
+++ wayat/frontend/lib/services/location/location_service_impl.dart
@@ -5,12 +5,18 @@ import 'location_service.dart';
 class LocationServiceImpl extends LocationService {
   @override
   ContactLocation getContactLocation(ContactLocation currentLocation) {
+    ContactLocation newLocation = currentLocation;
     Random random = Random();
     double moveMeters = random.nextDouble() * 10; //increments 0..10 meters move
 
-    ContactLocation newLocation = currentLocation;
-    newLocation.latitude = currentLocation.latitude + moveMeters;
-    newLocation.longitude = currentLocation.longitude + moveMeters;
+    double earth = 6378.137; //radius of the earth in kilometer
+    double m = (1 / ((2 * pi / 360) * earth)) / 1000; //1 meter in degree
+
+    //For latitude coordinates
+    newLocation.latitude = currentLocation.latitude + (moveMeters * m);
+    //For longtitude coordinates
+    newLocation.longitude = currentLocation.longitude +
+        (moveMeters * m) / cos(currentLocation.longitude * (pi / 180));
 
     return newLocation;
   }
