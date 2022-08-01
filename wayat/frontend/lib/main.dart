7c4c4bd557a26e7e418537eed1f7a4013c83eadd
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-08-01 11:50:31 +0200

    changed dotenv package

--- wayat/frontend/lib/main.dart
+++ wayat/frontend/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wayat/lang/lang_singleton.dart';
 import 'package:wayat/navigation/app_router.dart';
 
 Future main() async {
-  await dotenv.load(fileName: "development.env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+  
   registerRepositories();
   runApp(MyApp());
 }
