391c62d06ab122a8626a6dcfb4e25f22a6c88c08
Serhii Bets <serhii.bets@capgemini.com>
2022-08-01 18:48:09 +0200

    Clear home_map_page

--- wayat/frontend/lib/features/map/home_map_page.dart
+++ wayat/frontend/lib/features/map/home_map_page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -14,40 +12,9 @@ class HomeMapPageState extends State<HomeMapPage> {
     zoom: 14.4746,
   );
 
-  Completer<GoogleMapController> _mapController = Completer();
-  List<Marker> list = [];
-
-  void _onMapCreated(GoogleMapController controller) {
-    _mapController.complete(controller);
-  }
-
-  Set<Marker> _markers = {};
-  Marker marker1 = Marker(
-    markerId: MarkerId('Marker1'),
-    position: LatLng(32.195476, 74.2023563),
-    infoWindow: InfoWindow(title: 'Business 1'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-  );
-  Marker marker2 = Marker(
-    markerId: MarkerId('Marker2'),
-    position: LatLng(31.110484, 72.384598),
-    infoWindow: InfoWindow(title: 'Business 2'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-  );
-
-  @override
-  void initState() {
-    _markers.addAll(list);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
-      onMapCreated: (GoogleMapController controller) {
-        _mapController.complete(controller);
-      },
-      markers: Set<Marker>.of(list),
       initialCameraPosition: _valencia,
     );
   }
