c0a3227edbf383a6da005035b9124b6cbc8216a3
airherna <airam.hernandez-rocha@capgemini.com>
2022-08-04 09:33:50 +0200

    Images resized

--- wayat/frontend/lib/services/image_service/image_service.dart
+++ wayat/frontend/lib/services/image_service/image_service.dart
@@ -1,11 +1,26 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'dart:ui' as ui;
 
 class ImageService {
   Future<BitmapDescriptor> _getBitmapFromUrl(String url) async {
     http.Response response = await http.get(Uri.parse(url));
-    return BitmapDescriptor.fromBytes(response.bodyBytes);
+    Uint8List bytes = response.bodyBytes;
+    Uint8List resizedImage = await _resizeImage(bytes);
+    return BitmapDescriptor.fromBytes(resizedImage);
+  }
+
+  Future<Uint8List> _resizeImage(Uint8List bytes) async {
+    ui.Codec codec = await ui.instantiateImageCodec(bytes, targetWidth: 150);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    Uint8List resizedImage =
+        (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+            .buffer
+            .asUint8List();
+    return resizedImage;
   }
 
   Future<Map<String, BitmapDescriptor>> getBitmapsFromUrl(
