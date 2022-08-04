1398f4f5c43024f0111ceefb448193e1489e6b8b
airherna <airam.hernandez-rocha@capgemini.com>
2022-08-04 10:50:53 +0200

    circulos

--- wayat/frontend/lib/services/location/mock/contact_location_mock.dart
+++ wayat/frontend/lib/services/location/mock/contact_location_mock.dart
@@ -8,7 +8,7 @@ class ContactLocationMock {
       username: "active",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.46799126816723,
       longitude: -0.3559132477211037,
     ),
@@ -18,7 +18,7 @@ class ContactLocationMock {
       username: "pepe",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.487762537239135,
       longitude: -0.46046622208940335,
     ),
@@ -28,7 +28,7 @@ class ContactLocationMock {
       username: "pepeuser",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.444825299007185,
       longitude: -0.37600882598488544,
     ),
@@ -38,7 +38,7 @@ class ContactLocationMock {
       username: "name",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.456887507422074,
       longitude: -0.2858866045318938,
     ),
@@ -48,7 +48,7 @@ class ContactLocationMock {
       username: "display",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.52352332959457,
       longitude: -0.4156626035399936,
     ),
@@ -58,7 +58,7 @@ class ContactLocationMock {
       username: "test",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.49862516861606,
       longitude: -0.4026163391010844,
     ),
@@ -68,7 +68,7 @@ class ContactLocationMock {
       username: "testing",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.460333469009,
       longitude: -0.4506815238456867,
     ),
@@ -78,7 +78,7 @@ class ContactLocationMock {
       username: "testingtesting",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 37.5702,
       longitude: -0.786805,
     ),
@@ -88,7 +88,7 @@ class ContactLocationMock {
       username: "testingtest",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.1702,
       longitude: -0.686805,
     ),
@@ -98,7 +98,7 @@ class ContactLocationMock {
       username: "USERsecond",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 42.9702,
       longitude: -0.386805,
     ),
@@ -108,7 +108,7 @@ class ContactLocationMock {
       username: "third",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 43.9702,
       longitude: -0.486805,
     ),
@@ -118,7 +118,7 @@ class ContactLocationMock {
       username: "fourth",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 39.9702,
       longitude: -0.386805,
     ),
@@ -128,7 +128,7 @@ class ContactLocationMock {
       username: "fifth",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 36.2702,
       longitude: -0.686805,
     ),
@@ -138,7 +138,7 @@ class ContactLocationMock {
       username: "barbarian",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 44.9702,
       longitude: -0.586805,
     ),
@@ -148,7 +148,7 @@ class ContactLocationMock {
       username: "wise",
       email: "user@mail.com",
       imageUrl:
-          "https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png",
+          "https://gravatar.com/avatar/2f95d2d2103f7ad5245cc3b24f223943?s=400&d=mp&r=x",
       latitude: 42.3702,
       longitude: -0.786805,
     ),
