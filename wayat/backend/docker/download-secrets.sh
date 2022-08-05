14dcb67abc2df82b9ae546f4035fe27b64b60fa9
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-08-05 09:40:16 +0200

    Revert "Try to use download secrets script"
    This reverts commit 94a8502365bb5b0995883c2a311c503c514b81eb.

--- wayat/backend/docker/download-secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget -O firebase.json https://storage.cloud.google.com/wayat_secrets/firebase.json
-if [[ $? -ne 0 ]]; then
-    echo "Failed to download Firebase Credentials"
-    exit 1; 
-fi
