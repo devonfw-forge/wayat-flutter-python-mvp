4744ed88f810262ba8abf037ce2818eac3fd3873
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-05 14:18:11 +0200

    Create User Repository

---
+++ wayat/backend/app/domain/wayat_management/models/user.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from app.common.base.base_firebase_repository import BaseFirebaseModel
+
+
+class UserEntity(BaseFirebaseModel):
+    name: Optional[str]
+    surname: Optional[str]
+    email: str
+    phone: Optional[str]
+    image_url: Optional[str]
+    do_not_disturb: bool = False
