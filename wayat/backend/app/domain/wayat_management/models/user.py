5eb8a4768e52cffbce820a04d0bcf31cddc61d80
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-08 10:54:48 +0200

    Initialize contacts list as empty

--- wayat/backend/app/domain/wayat_management/models/user.py
+++ wayat/backend/app/domain/wayat_management/models/user.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from pydantic import Field
+
 from app.common.base.base_firebase_repository import BaseFirebaseModel
 
 
@@ -10,3 +12,4 @@ class UserEntity(BaseFirebaseModel):
     image_url: Optional[str]
     do_not_disturb: bool = False
     onboarding_completed: bool = False
+    contacts: list = Field(default_factory=list)
