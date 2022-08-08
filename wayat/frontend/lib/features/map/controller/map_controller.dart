98e78defae0d9dd12bd6e51816e4da8f3663d427
Serhii Bets <serhii.bets@capgemini.com>
2022-08-08 10:35:16 +0200

    Add update contacts markers func

--- wayat/frontend/lib/features/map/controller/map_controller.dart
+++ wayat/frontend/lib/features/map/controller/map_controller.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:mobx/mobx.dart';
-import 'package:wayat/domain/contact/contact.dart';
 import 'package:wayat/domain/location/contact_location.dart';
 import 'package:wayat/services/image_service/image_service.dart';
+import 'package:wayat/services/location/location_service_impl.dart';
 import 'package:wayat/services/location/mock/contact_location_mock.dart';
 
 part 'map_controller.g.dart';
@@ -46,6 +46,30 @@ abstract class _MapController with Store {
         .toSet();
 
     setMarkers(newMarkers);
+  }
+
+  void updateMarkers() async {
+    // ignore: avoid_function_literals_in_foreach_calls
+    contacts.forEach((contact) async {
+      LatLng currentPosition = LatLng(contact.latitude, contact.longitude);
+      LatLng newPosition =
+          LocationServiceImpl().changeContactCoordinates(currentPosition);
+
+      Map<String, BitmapDescriptor> bitmaps = await imageService
+          .getBitmapsFromUrl(contacts.map((e) => e.imageUrl).toList());
+      Set<Marker> newMarkers = contacts
+          .map(
+            (e) => Marker(
+                markerId: MarkerId(e.displayName +
+                    e.longitude.toString() +
+                    e.latitude.toString()),
+                position: newPosition,
+                icon: bitmaps[e.imageUrl]!,
+                onTap: () => onMarkerPressed(e, bitmaps[e.imageUrl]!)),
+          )
+          .toSet();
+      setMarkers(newMarkers);
+    });
   }
 
   @action
