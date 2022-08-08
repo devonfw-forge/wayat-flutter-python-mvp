d361b949ff983627ec3a4a54f84140615f7389a5
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-08-08 14:06:54 +0200

    Create Status Repository

---
+++ wayat/backend/app/domain/wayat_management/repositories/status.py
@@ -0,0 +1,10 @@
+from fastapi import Depends
+
+from app.common.base.base_firebase_repository import BaseFirestoreRepository, get_async_client
+from app.domain.wayat_management.models.status import AppStatusEntity
+from google.cloud.firestore import AsyncClient
+
+
+class StatusRepository(BaseFirestoreRepository[AppStatusEntity]):
+    def __init__(self, client: AsyncClient = Depends(get_async_client)):
+        super(StatusRepository, self).__init__(collection_path="status", model=AppStatusEntity, client=client)
