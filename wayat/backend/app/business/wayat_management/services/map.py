eb1a2549d35f92a3ce0357e02c908e2b7d2b4aa1
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-10 14:57:49 +0200

    Parametrize update threshold

--- wayat/backend/app/business/wayat_management/services/map.py
+++ wayat/backend/app/business/wayat_management/services/map.py
@@ -1,20 +1,38 @@
 import asyncio
 from datetime import datetime
+from functools import lru_cache
 
 from fastapi import Depends
+from pydantic import BaseSettings
 
+from app.common.core.configuration import load_env_file_on_settings
 from app.domain.wayat_management.models.status import ContactRefInfo
 from app.domain.wayat_management.repositories.status import StatusRepository
 from app.domain.wayat_management.repositories.user import UserRepository
 
 
+class MapSettings(BaseSettings):
+    update_threshold: int
+
+    class Config:
+        env_prefix = "MAP_"
+        env_file = "TEST.env"
+
+
+@lru_cache
+def get_map_settings() -> MapSettings:
+    return load_env_file_on_settings(MapSettings)
+
+
 class MapService:
     def __init__(self,
                  user_repository: UserRepository = Depends(),
-                 status_repository: StatusRepository = Depends()):
+                 status_repository: StatusRepository = Depends(),
+                 map_settings: MapSettings = Depends(get_map_settings)
+                 ):
         self._user_repository = user_repository
         self._status_repository = status_repository
-        self._update_threshold = 30  # minimum seconds between update
+        self._update_threshold = map_settings.update_threshold  # minimum seconds between updates
 
     async def update_location(self,
                               uid: str,
