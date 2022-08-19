c118723636da10be3959213bee8af2a83c151762
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-19 13:50:24 +0200

    Create run_in_executor decorator

--- wayat/backend/app/common/infra/gcp/cloud_storage.py
+++ wayat/backend/app/common/infra/gcp/cloud_storage.py
@@ -1,15 +1,17 @@
 import asyncio
 import datetime
+import functools
 import json
 import mimetypes
 from functools import lru_cache
-from typing import BinaryIO
+from typing import BinaryIO, ParamSpec, TypeVar, Callable, Awaitable
 
 from google.cloud.storage import Client, Bucket
 from pydantic import BaseSettings
 
 from app.common.core.configuration import load_env_file_on_settings
-from app.common.utils import get_current_time
+from app.domain.wayat_management.utils import get_current_time
+
 
 class BaseStorageSettings(BaseSettings):
     credentials_file: str
@@ -37,6 +39,22 @@ def _get_storage_client():
     return Client.from_service_account_info(_get_account_info())
 
 
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def run_in_executor(f: Callable[P, R]) -> Callable[P, Awaitable[R]]:
+
+    async def async_f(*args: P.args, **kwargs: P.kwargs):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(
+            executor=None,
+            func=functools.partial(f, *args, **kwargs)
+        )
+
+    return async_f
+
+
 class CloudStorage:
     def __init__(self,
                  client: Client,
@@ -47,23 +65,19 @@ class CloudStorage:
     def _bucket(self) -> Bucket:
         return self._client.bucket(self._configuration.bucket)
 
+    @run_in_executor
+    def _upload_image(self, filename: str, data: BinaryIO | bytes) -> str:
+        content_type = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+        path = f"{self._configuration.images_path}/{filename}"
+        blob = self._bucket().blob(path)
+        if isinstance(data, bytes):
+            blob.upload_from_string(data, content_type=content_type)
+        else:
+            blob.upload_from_file(data, content_type=content_type)
+        return blob.name
+
     async def upload_image(self, filename: str, data: BinaryIO | bytes) -> str:
-        def sync_upload():
-            content_type = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-            path = f"{self._configuration.images_path}/{filename}"
-            blob = self._bucket().blob(path)
-            if isinstance(data, bytes):
-                blob.upload_from_string(data, content_type=content_type)
-            else:
-                blob.upload_from_file(data, content_type=content_type)
-            return blob.name
-
-        loop = asyncio.get_event_loop()
-
-        return await loop.run_in_executor(
-            executor=None,
-            func=sync_upload
-        )
+        return await self._upload_image(filename, data)
 
     def generate_signed_url(self, reference: str | None):
         if reference:
@@ -73,3 +87,18 @@ class CloudStorage:
 
     def _expiration_time(self) -> datetime.datetime:
         return get_current_time() + datetime.timedelta(seconds=self._configuration.expiration_time)
+
+    @run_in_executor
+    def _delete(self, reference: str | None = None, prefix: str | None = None):
+        if reference:
+            blob_iterator = [self._bucket().blob(reference)]
+        elif prefix:
+            blob_iterator = self._client.list_blobs(self._bucket(), prefix=prefix)
+        else:
+            raise ValueError("Either reference or prefix must be defined")
+
+        for blob in blob_iterator:
+            blob.delete()
+
+    async def delete(self, reference: str | None = None, prefix: str | None = None):
+        await self._delete(reference, prefix)
