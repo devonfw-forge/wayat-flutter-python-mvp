11b39a47080c556acc103ef3f59db2c6aa282b00
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-26 12:08:33 +0200

    Add request and response models

--- wayat/backend/app/business/wayat_management/controllers/group.py
+++ wayat/backend/app/business/wayat_management/controllers/group.py
@@ -1,7 +1,10 @@
 import logging
+from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile
 
+from app.business.wayat_management.models.group import CreateGroupRequest, UpdateGroupRequest, ListGroupsResponse, \
+    CreateGroupResponse
 from app.common import get_user
 from app.common.infra.gcp.firebase import FirebaseAuthenticatedUser
 
@@ -10,20 +13,23 @@ router = APIRouter(prefix="/groups")
 logger = logging.getLogger(__name__)
 
 
-@router.get("/", description="Get the list of groups of a user")
-async def list_groups(user: FirebaseAuthenticatedUser = Depends(get_user())):
+@router.get("/", description="Get the list of groups of a user. "
+                             "An optional 'group_id' query parameter allows retrieving the selected group",
+            response_model=ListGroupsResponse)
+async def list_groups(group_id: Optional[str] = None, user: FirebaseAuthenticatedUser = Depends(get_user())):
     # TODO: Implement this method
     raise NotImplementedError
 
 
-@router.post("/", description="Create a group")
-async def create_group(user: FirebaseAuthenticatedUser = Depends(get_user())):
+@router.post("/", description="Create a group", response_model=CreateGroupResponse)
+async def create_group(request: CreateGroupRequest, user: FirebaseAuthenticatedUser = Depends(get_user())):
     # TODO: Implement this method
     raise NotImplementedError
 
 
 @router.put("/{group_id}", description="Update the information of a group")
-async def update_group(group_id: str, user: FirebaseAuthenticatedUser = Depends(get_user())):
+async def update_group(group_id: str, request: UpdateGroupRequest,
+                       user: FirebaseAuthenticatedUser = Depends(get_user())):
     # TODO: Implement this method
     raise NotImplementedError
 
@@ -35,6 +41,7 @@ async def delete_group(group_id: str, user: FirebaseAuthenticatedUser = Depends(
 
 
 @router.post("/{group_id}/picture", description="Update the profile picture of a group")
-async def upload_group_picture(user: FirebaseAuthenticatedUser = Depends(get_user())):
+async def upload_group_picture(group_id: str, upload_file: UploadFile,
+                               user: FirebaseAuthenticatedUser = Depends(get_user())):
     # TODO: Implement this method
     raise NotImplementedError
