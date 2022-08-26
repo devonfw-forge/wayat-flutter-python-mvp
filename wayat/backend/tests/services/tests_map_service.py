ba8a228c1612174d5c110fa2c2cd5a3fb34e51e4
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-08-26 16:16:02 +0200

    Test update_location_should_update_all_contacts_with_map_open

--- wayat/backend/tests/services/tests_map_service.py
+++ wayat/backend/tests/services/tests_map_service.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone, timedelta
 from unittest import IsolatedAsyncioTestCase, skip
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 
 from app.business.wayat_management.services.map import MapService, MapSettings
 from app.common.infra.gcp.base_firebase_repository import GeoPoint
+from app.domain.wayat_management.models.status import ContactRefInfo
 from app.domain.wayat_management.models.user import UserEntity, Location
 from app.domain.wayat_management.repositories.status import StatusRepository
 from app.domain.wayat_management.repositories.user import UserRepository
@@ -49,6 +50,13 @@ class MapServiceTests(IsolatedAsyncioTestCase):
             )
         }
 
+    async def test_update_location_should_update_location_in_repository(self):
+        # Call to test
+        await self.map_service.update_location("uid_1", 0.1, 0.2, "Gulf of Guinea")
+
+        # Asserts
+        self.mock_user_repo.update_user_location.assert_called_with("uid_1", 0.1, 0.2, "Gulf of Guinea")
+
     async def test_update_location_when_no_map_open_should_not_set_active(self):
         # Mocking
         self.mock_user_repo.find_contacts_with_map_open.return_value = []
@@ -69,7 +77,7 @@ class MapServiceTests(IsolatedAsyncioTestCase):
         # Mocking
         # User2 is aprox 3 km away from (0,0)
         self.mock_entities["user_2"].location = Location(
-            last_updated=datetime.now(),
+            last_updated=datetime.now(timezone.utc),
             value=GeoPoint.validate_geopoint((0, 0.03)),
             address="Gulf of Guinea",
         )
@@ -91,7 +99,7 @@ class MapServiceTests(IsolatedAsyncioTestCase):
         # Mocking
         # User2 is aprox 1 km away from (0,0)
         self.mock_entities["user_2"].location = Location(
-            last_updated=datetime.now(),
+            last_updated=datetime.now(timezone.utc),
             value=GeoPoint.validate_geopoint((0, 0.01)),
             address="Gulf of Guinea",
         )
@@ -109,26 +117,25 @@ class MapServiceTests(IsolatedAsyncioTestCase):
         # Asserts
         self.mock_status_repo.set_active.assert_called_with(uid, True)
 
-    async def test_update_location_should_update_location_in_repository(self):
-        # Call to test
-        await self.map_service.update_location("uid_1", 0.1, 0.2, "Gulf of Guinea")
-
-        # Asserts
-        self.mock_user_repo.update_user_location.assert_called_with("uid_1", 0.1, 0.2, "Gulf of Guinea")
-
-    @skip("Needs fixing")
     @patch("app.business.wayat_management.services.map.datetime")
     async def test_update_location_should_update_all_contacts_with_map_open(self, datetime_mock):
         # Mocks
         # User2 and User3 have the map open
         # User2 is in range, User3 isn't. However, both should receive the update
-        self.mock_entities["user_2"].location = Location(
-            last_updated=datetime.now(),
-            value=GeoPoint.validate_geopoint((0, 0.01))
+        self.mock_entities["user_1"].location = Location(
+            last_updated=datetime.now(timezone.utc),
+            value=GeoPoint.validate_geopoint((0, 0.01)),
+            address="TestLocationUser1"
         )
         self.mock_entities["user_2"].location = Location(
-            last_updated=datetime.now(),
-            value=GeoPoint.validate_geopoint((0, 0.03))
+            last_updated=datetime.now(timezone.utc),
+            value=GeoPoint.validate_geopoint((0, 0.01)),
+            address="TestLocationUser2"
+        )
+        self.mock_entities["user_3"].location = Location(
+            last_updated=datetime.now(timezone.utc),
+            value=GeoPoint.validate_geopoint((0, 0.03)),
+            address="TestLocationUser3"
         )
         # Mock the last_status_update field so that both contacts are updated
         now_timestamp = datetime(2022, 8, 16, 12, 0, 0, tzinfo=timezone.utc)
@@ -136,11 +143,27 @@ class MapServiceTests(IsolatedAsyncioTestCase):
         self.mock_entities["user_2"].last_status_update = old_enough_timestamp
         self.mock_entities["user_3"].last_status_update = old_enough_timestamp
         datetime_mock.now.return_value = now_timestamp
+
+        def mocking_get_user_location(user_id: str):
+            return self.mock_entities[user_id].location
+
         # Mock the user repo
         self.mock_user_repo.find_contacts_with_map_open.return_value = [self.mock_entities["user_2"],
                                                                         self.mock_entities["user_3"]]
+        self.mock_user_repo.get_user_location.side_effect = mocking_get_user_location
+
         uid = self.mock_entities["user_1"].document_id
+
+        # Expected calls args
+        contact_refs_1 = [ContactRefInfo(uid=self.mock_entities["user_1"].document_id,
+                                         last_updated=self.mock_entities["user_1"].location.last_updated,
+                                         location=self.mock_entities["user_1"].location.value,
+                                         address=self.mock_entities["user_1"].location.address)]
+
         # Call under test
-        await self.map_service.update_location(uid, 0.0, 0.0)
+        await self.map_service.update_location(uid, 0.0, 0.0, "Test")
+
         # Asserts
-        self.mock_status_repo.assert_has_calls([("uid", "user_2"), ("uid", "user_3")], any_order=True)
+        self.mock_user_repo.update_last_status.assert_has_calls([call("user_2"), call("user_3")], any_order=True)
+        self.mock_status_repo.set_contact_refs.assert_has_calls([call("user_2", contact_refs_1),
+                                                                 call("user_3", contact_refs_1)], any_order=True)
