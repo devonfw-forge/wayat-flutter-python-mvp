0b48aa94a286ba1c6d29b55b222e0441f4c29d61
airherna <airam.hernandez-rocha@capgemini.com>
2022-08-29 09:32:37 +0200

    centralized api contract & changed in all services

--- wayat/frontend/lib/services/friend_requests/requests_service_impl.dart
+++ wayat/frontend/lib/services/friend_requests/requests_service_impl.dart
@@ -1,58 +1,66 @@
 import 'package:wayat/domain/contact/contact.dart';
+import 'package:wayat/services/api_contract/api_contract.dart';
 import 'package:wayat/services/friend_requests/requests_service.dart';
 
 class RequestsServiceImpl extends RequestsService {
-  
   @override
   Future<Map<String, List<Contact>>> getRequests() async {
-    Map<String, dynamic> friendRequests = await super.sendGetRequest("users/friend-requests");
-    
-    List<Contact> pendingRequests = (friendRequests["pending_requests"] as List).map((e) {
+    Map<String, dynamic> friendRequests =
+        await super.sendGetRequest(APIContract.friendRequests);
+
+    List<Contact> pendingRequests =
+        (friendRequests["pending_requests"] as List).map((e) {
       return Contact.fromMap(e);
     }).toList();
 
-    List<Contact> sentRequests = (friendRequests["sent_requests"] as List).map((e) {
+    List<Contact> sentRequests =
+        (friendRequests["sent_requests"] as List).map((e) {
       return Contact.fromMap(e);
     }).toList();
 
-    return {
-      "pending_requests": pendingRequests,
-      "sent_requests": sentRequests
-    };
+    return {"pending_requests": pendingRequests, "sent_requests": sentRequests};
   }
 
   @override
   Future<bool> sendRequests(List<Contact> contacts) async {
-    return (await super.sendPostRequest(
-      "users/add-contact", {"users": contacts.map((e) => e.id).toList()}))
-        .statusCode / 10 == 20;
+    return (await super.sendPostRequest(APIContract.addContact,
+                    {"users": contacts.map((e) => e.id).toList()}))
+                .statusCode /
+            10 ==
+        20;
   }
 
   @override
   Future<bool> acceptRequest(Contact contact) async {
-    return (await super.sendPostRequest("users/friend-requests", {
-      "uid": contact.id,
-      "accept": true
-    })).statusCode / 10 == 20;
+    return (await super.sendPostRequest(APIContract.friendRequests,
+                    {"uid": contact.id, "accept": true}))
+                .statusCode /
+            10 ==
+        20;
   }
 
   @override
   Future<bool> rejectRequest(Contact contact) async {
-    return (await super.sendPostRequest("users/friend-requests", {
-      "uid": contact.id,
-      "accept": false
-    })).statusCode / 10 == 20;
+    return (await super.sendPostRequest(APIContract.friendRequests,
+                    {"uid": contact.id, "accept": false}))
+                .statusCode /
+            10 ==
+        20;
   }
 
   @override
   Future<bool> sendRequest(Contact contact) async {
-    return (await super.sendPostRequest(
-      "users/add-contact", {"users": [contact.id]}))
-        .statusCode / 10 == 20;
+    return (await super.sendPostRequest(APIContract.addContact, {
+              "users": [contact.id]
+            }))
+                .statusCode /
+            10 ==
+        20;
   }
 
   @override
   Future<bool> unsendRequest(Contact contact) async {
-    return await super.sendDelRequest("users/friend-requests/sent/${contact.id}");
+    return await super
+        .sendDelRequest("${APIContract.sentFriendRequests}/${contact.id}");
   }
 }
