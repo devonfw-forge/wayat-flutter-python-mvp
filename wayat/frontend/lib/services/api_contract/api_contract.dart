507589d5a07496fc2fa5732bd6403941c24b9fe6
airherna <airam.hernandez-rocha@capgemini.com>
2022-08-29 11:48:55 +0200

    Update api_contract.dart

--- wayat/frontend/lib/services/api_contract/api_contract.dart
+++ wayat/frontend/lib/services/api_contract/api_contract.dart
@@ -1,10 +1,12 @@
 class APIContract {
   /*
-   * USER 
+   * ============ USER ============ *
    */
   /// [GET] Returns User profile
   ///
   /// [POST] Updates User
+  ///
+  /// [DELETE] Delete User account
   static const String userProfile = "users/profile";
 
   /// [POST] Updates user profile picture (requires Multipart File)
@@ -13,27 +15,47 @@ class APIContract {
   /// [POST] Returns wayat accounts based on a list of phones
   static const String findByPhone = "users/find-by-phone";
 
-  /// [POST] Sends friends requests to a list of contacts
-  static const String addContact = "users/add-contact";
-
   /// [POST] Updates share location and do not disturb preferences
   static const String preferences = "users/preferences";
+
+  /*
+   * ============ CONTACT ============ *
+   */
+
+  /// [POST] Sends friends requests to a list of contacts
+  static const String addContact = "contacts/add";
 
   /// [GET] Returns the list of contacts for a user
   ///
   /// [DELETE] Deletes a contact (Requires ID appended in URL)
-  static const String contacts = "users/contacts";
+  static const String contacts = "contacts";
 
   /// [GET] Returns sent and received friendship requests
   ///
   /// [POST] Accepts or denies a friend request
-  static const String friendRequests = "users/friend-requests";
+  static const String friendRequests = "contacts/requests";
 
   /// [DELETE] Cancels a sent friend request (Requires ID appended)
-  static const String sentFriendRequests = "users/friend-requests/sent";
+  static const String sentFriendRequests = "contacts/requests/sent";
 
   /*
-   * MAP 
+   * ============ GROUP ============ *
+   */
+
+  /// [GET] Returns this user's groups
+  ///
+  /// [POST] Creates a group
+  ///
+  /// [PUT] Updates a group (Required ID appended in the URL)
+  ///
+  /// [DELETE] Deletes a group (Required ID appended in the URL)
+  static const String groups = "groups";
+
+  /// [POST] Updates the group's picture (Required ID appended in the URL)
+  static const String groupPicture = "groups/picture";
+
+  /*
+   * ============ MAP ============ *
    */
   /// [POST] Updates your location
   static const String updateLocation = "map/update-location";
