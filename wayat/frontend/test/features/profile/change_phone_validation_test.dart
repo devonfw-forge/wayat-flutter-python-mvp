812daf807663eab1cf864e27bae6cfa709b9e98b
Serhii Bets <serhii.bets@capgemini.com>
2022-09-02 15:19:37 +0200

    ignore

--- wayat/frontend/test/features/profile/change_phone_validation_test.dart
+++ wayat/frontend/test/features/profile/change_phone_validation_test.dart
@@ -1,21 +1,23 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/mockito.dart';
 import 'package:wayat/app_state/location_state/location_state.dart';
 import 'package:wayat/app_state/profile_state/profile_state.dart';
 import 'package:wayat/app_state/user_session/session_state.dart';
+import 'package:wayat/common/widgets/buttons/filled_button.dart';
+import 'package:wayat/common/widgets/buttons/text_button.dart';
 import 'package:wayat/common/widgets/card.dart';
 import 'package:wayat/common/widgets/switch.dart';
 import 'package:wayat/domain/user/my_user.dart';
+import 'package:wayat/features/profile/pages/change_phone_page/change_phone_validation_page.dart';
 import 'package:wayat/features/profile/pages/profile_page.dart';
 import 'package:wayat/lang/app_localizations.dart';
 import 'package:wayat/lang/lang_singleton.dart';
 import 'package:mockito/annotations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'profile_test.mocks.dart';
 
 @GenerateMocks([SessionState, ProfileState, LocationState])
@@ -25,10 +27,11 @@ void main() async {
   final MockLocationState mockLocationState = MockLocationState();
   late MyUser user;
 
-  setUpAll(() {
+  setUpAll(() async {
+    await dotenv.load(fileName: ".env");
     HttpOverrides.global = null;
     user = MyUser(
-        id: "2",
+        id: "5",
         name: "test",
         email: "test@capg.com",
         imageUrl: "http://example.com",
@@ -57,41 +60,35 @@ void main() async {
     );
   }
 
-  testWidgets('Profile page has Profile label', (tester) async {
-    await tester.pumpWidget(_createApp(ProfilePage()));
-    expect(find.widgetWithText(ListView, appLocalizations.profile),
-        findsOneWidget);
-  });
-
-  group("Profile page has user profile data", () {
-    testWidgets('Profile image', (tester) async {
-      await tester.pumpWidget(_createApp(ProfilePage()));
-      expect(find.byKey(const Key("profile_image")), findsOneWidget);
+  group("Change phone validation page has correct components", () {
+    testWidgets('Change phone validation page title', (tester) async {
+      await tester.pumpWidget(_createApp(ChangePhoneValidationPage(
+        newPhoneNumber: '+34777777777',
+      )));
+      expect(find.widgetWithText(Text, appLocalizations.verifyPhoneTitle),
+          findsOneWidget);
     });
 
-    testWidgets('Username', (tester) async {
-      await tester.pumpWidget(_createApp(ProfilePage()));
-      expect(find.text(user.name), findsOneWidget);
-    });
-  });
-
-  group("Share location settings UI", () {
-    testWidgets('Settings title', (tester) async {
-      await tester.pumpWidget(_createApp(ProfilePage()));
-      expect(find.text(appLocalizations.profileShareLocation), findsOneWidget);
+    testWidgets('Change phone validation page text', (tester) async {
+      await tester.pumpWidget(_createApp(ChangePhoneValidationPage(
+        newPhoneNumber: '+34888888888',
+      )));
+      expect(find.widgetWithText(Text, appLocalizations.verifyPhoneText),
+          findsOneWidget);
     });
 
-    testWidgets('Switch active location', (tester) async {
-      await tester.pumpWidget(_createApp(ProfilePage()));
-      expect(find.text(appLocalizations.profileActiveLocation), findsOneWidget);
-      expect(find.byType(CustomSwitch), findsOneWidget);
+    testWidgets('Change phone validation page Verify button', (tester) async {
+      await tester.pumpWidget(_createApp(ChangePhoneValidationPage(
+        newPhoneNumber: '+34999999999',
+      )));
+      expect(find.byType(CustomFilledButton), findsOneWidget);
     });
-  });
 
-  testWidgets('Edit profile button', (tester) async {
-    await tester.pumpWidget(_createApp(ProfilePage()));
-    expect(find.text(appLocalizations.profileActiveLocation), findsOneWidget);
-    expect(find.widgetWithText(CustomCard, appLocalizations.editProfile),
-        findsOneWidget);
+    testWidgets('Change phone validation page Cancel button', (tester) async {
+      await tester.pumpWidget(_createApp(ChangePhoneValidationPage(
+        newPhoneNumber: '+34000000000',
+      )));
+      expect(find.byType(CustomTextButton), findsOneWidget);
+    });
   });
 }
