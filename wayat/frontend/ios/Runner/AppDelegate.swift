982a5d534e9947a67d4dc07722cc1b289b85b9d3
airherna <101186485+airherna@users.noreply.github.com>
2022-09-05 15:20:38 +0200

    Revert "238 delete account modified"

--- wayat/frontend/ios/Runner/AppDelegate.swift
+++ wayat/frontend/ios/Runner/AppDelegate.swift
@@ -1,18 +1,13 @@
 import UIKit
 import Flutter
-import GoogleMaps
-
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCEILvlgyqTS4Kwv5Ed3lKf6ZFHX_1HgTg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  
 }
