468d22d0b03a780d332e4db13b2f6e62adffdeab
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-06 12:06:00 +0200

    ContactService & RequestService tests

--- wayat/frontend/lib/services/contact/contact_service.dart
+++ wayat/frontend/lib/services/contact/contact_service.dart
@@ -3,7 +3,7 @@ import 'package:wayat/domain/contact/contact.dart';
 abstract class ContactService {
   Future<List<Contact>> getAll();
 
-  void sendRequests(List<Contact> contacts);
+  Future<void> sendRequests(List<Contact> contacts);
 
   Future<List<Contact>> getFilteredContacts(List<String> importedContacts);
 
