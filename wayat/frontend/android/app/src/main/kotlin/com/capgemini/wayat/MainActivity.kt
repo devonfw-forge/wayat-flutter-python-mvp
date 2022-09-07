365dff36f54240b63d8cdcc696d1d20ae3e8954e
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-07 16:37:20 +0200

    fixed contact tapping error

--- wayat/frontend/android/app/src/main/kotlin/com/capgemini/wayat/MainActivity.kt
+++ wayat/frontend/android/app/src/main/kotlin/com/capgemini/wayat/MainActivity.kt
@@ -1,6 +1,28 @@
 package com.capgemini.wayat
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.view.FlutterMain
+import android.os.Build;
+import android.view.WindowManager
+import android.util.Log;
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.MapsInitializer.Renderer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), OnMapsSdkInitializedCallback {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState);
+        MapsInitializer.initialize(applicationContext, Renderer.LATEST, this)
+    }
+
+    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
+      when (renderer) {
+        Renderer.LATEST -> Log.d("NewRendererLog", "The latest version of the renderer is used.")
+        Renderer.LEGACY -> Log.d("NewRendererLog","The legacy version of the renderer is used.")
+      }
+    }
 }
