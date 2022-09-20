86c3ed67fa355a972fd07befd1e59dd86d2f697e
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-09-20 15:17:10 +0200

    Script to get report excluding unneded files

---
+++ wayat/frontend/test_coverage.ps1
@@ -0,0 +1,7 @@
+flutter clean
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter test --coverage
+flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
+flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.gr.dart$'
+flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\\notifications'
