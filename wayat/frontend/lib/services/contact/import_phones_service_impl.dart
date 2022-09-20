f09f17d920fa110b21e4d0cb25f9d2a4ee7e9a0b
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-20 15:31:33 +0200

    removed simultaneous request to the service

--- wayat/frontend/lib/services/contact/import_phones_service_impl.dart
+++ wayat/frontend/lib/services/contact/import_phones_service_impl.dart
@@ -2,17 +2,13 @@ import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactsAddressServiceImpl {
   static Future<List<String>> getAllPhones() async {
-    try {
-      if (await FlutterContacts.requestPermission(readonly: true)) {
-        List<Contact> contacts =
-            await FlutterContacts.getContacts(withProperties: true);
-        return contacts
-            .expand((contact) => contact.phones.map((e) => e.number))
-            .map((e) => e.toString())
-            .toList();
-      }
-    } catch (e) {
-      return [];
+    if (await FlutterContacts.requestPermission(readonly: true)) {
+      List<Contact> contacts =
+          await FlutterContacts.getContacts(withProperties: true);
+      return contacts
+          .expand((contact) => contact.phones.map((e) => e.number))
+          .map((e) => e.toString())
+          .toList();
     }
     return [];
   }
