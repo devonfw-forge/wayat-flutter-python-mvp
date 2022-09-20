b1c03f0e6fa8a061eda89bfa4ed332305aecb7ff
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-09-20 15:21:41 +0200

    change name of script

--- wayat/frontend/generate_coverage.ps1
+++ wayat/frontend/generate_coverage.ps1
@@ -4,4 +4,3 @@ flutter pub run build_runner build --delete-conflicting-outputs
 flutter test --coverage
 flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'
 flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.gr.dart$'
-flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\\notifications'
