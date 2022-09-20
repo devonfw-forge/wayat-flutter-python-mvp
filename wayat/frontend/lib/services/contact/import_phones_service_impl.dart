8025c352a352d70fb81f0284c21919356d7ffe2d
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-20 12:10:46 +0200

    Update import_phones_service_impl.dart

--- wayat/frontend/lib/services/contact/import_phones_service_impl.dart
+++ wayat/frontend/lib/services/contact/import_phones_service_impl.dart
@@ -2,13 +2,18 @@ import 'package:flutter_contacts/flutter_contacts.dart';
 
 class ContactsAddressServiceImpl {
   static Future<List<String>> getAllPhones() async {
-    if (await FlutterContacts.requestPermission()) {
-      List<Contact> contacts = await FlutterContacts.getContacts(withProperties: true);
-      return contacts
-          .expand((contact) => contact.phones.map((e) => e.number))
-          .map((e) => e.toString())
-          .toList();
+    try {
+      if (await FlutterContacts.requestPermission(readonly: true)) {
+        List<Contact> contacts =
+            await FlutterContacts.getContacts(withProperties: true);
+        return contacts
+            .expand((contact) => contact.phones.map((e) => e.number))
+            .map((e) => e.toString())
+            .toList();
+      }
+    } catch (e) {
+      return [];
     }
-    return List.empty();
+    return [];
   }
 }
