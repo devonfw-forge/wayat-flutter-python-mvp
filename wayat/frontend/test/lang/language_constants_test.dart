4b689cb54d73bdf5b59676a82e5f47f80d92e8c9
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-09-22 13:51:44 +0200

    improvement test

--- wayat/frontend/test/lang/language_constants_test.dart
+++ wayat/frontend/test/lang/language_constants_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -6,22 +5,24 @@ import 'package:wayat/lang/language_constants.dart';
 
 void main() async {
 
+  test("Generate Locale correctly", () {
+    expect(locale('en'), const Locale("en", "US"));
+
+    expect(locale('es'), const Locale("es", "ES"));
+
+    expect(locale('fr'), const Locale("fr", "FR"));
+
+    expect(locale('de'), const Locale("de", "DE"));
+
+    expect(locale('nl'), const Locale("nl", "NL"));
+
+    expect(locale('unkown'), const Locale("en", "US"));
+  });
+  
   test("setLocaleConstants save the language on sharePreferences", () async {
-    setLocaleConstants('es');
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    setLocaleConstants('es');
     expect(prefs.getString('languageCode'), 'es');
-    setLocaleConstants('en');
-    prefs = await SharedPreferences.getInstance();
-    expect(prefs.getString('languageCode'), 'en');
-    setLocaleConstants('fr');
-    prefs = await SharedPreferences.getInstance();
-    expect(prefs.getString('languageCode'), 'fr');
-    setLocaleConstants('nl');
-    prefs = await SharedPreferences.getInstance();
-    expect(prefs.getString('languageCode'), 'nl');
-    setLocaleConstants('de');
-    prefs = await SharedPreferences.getInstance();
-    expect(prefs.getString('languageCode'), 'de');
   });
 
   test("getLocaleConstants get the language of sharePreferences", () async {
