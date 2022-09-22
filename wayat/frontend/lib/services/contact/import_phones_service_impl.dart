82da12c6afc8f650b6ae83ec578ae26fdddfc0db
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-22 16:18:02 +0200

    Improved coverage a bit by encapsulating FlutterContacts calls

--- wayat/frontend/lib/services/contact/import_phones_service_impl.dart
+++ wayat/frontend/lib/services/contact/import_phones_service_impl.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:wayat/services/contact/flutter_contacts_handler.dart';
 
 class ContactsAddressServiceImpl {
-  static Future<List<String>> getAllPhones() async {
-    if (await FlutterContacts.requestPermission(readonly: true)) {
-      List<Contact> contacts =
-          await FlutterContacts.getContacts(withProperties: true);
+  static Future<List<String>> getAllPhones(
+      {FlutterContactsHandler? handler}) async {
+    FlutterContactsHandler contactsHandler =
+        handler ?? FlutterContactsHandler();
+    if (await contactsHandler.requestPermission()) {
+      List<Contact> contacts = await contactsHandler.getContacts();
       return contacts
           .expand((contact) => contact.phones.map((e) => e.number))
           .map((e) => e.toString())
