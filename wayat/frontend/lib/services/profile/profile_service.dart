81a65aa6f9e2b8a4c89fe5aa0ec867f599edc47d
Serhii Bets <serhii.bets@capgemini.com>
2022-09-27 12:04:40 +0200

    Add dartdocs comments to profile_service and profile_service_impl

--- wayat/frontend/lib/services/profile/profile_service.dart
+++ wayat/frontend/lib/services/profile/profile_service.dart
@@ -3,5 +3,7 @@ import 'package:image_picker/image_picker.dart';
 abstract class ProfileService {
   Future<bool> uploadProfileImage(XFile? selectedImage);
   Future<bool> updateProfileName(String name);
+
+  /// *Delete* current user process
   Future<bool> deleteCurrentUser();
 }
--- wayat/frontend/lib/services/profile/profile_service_impl.dart
+++ wayat/frontend/lib/services/profile/profile_service_impl.dart
@@ -10,9 +10,11 @@ import 'package:mime/mime.dart';
 class ProfileServiceImpl implements ProfileService {
   final HttpProvider httpProvider = GetIt.I.get<HttpProvider>();
 
+  ///Update profile image from camera or gallery [selectedImage]
+  ///
+  ///send [POST] response to backend to update user profile image
   @override
   Future<bool> uploadProfileImage(XFile? selectedImage) async {
-    //Uint8List bytes = await io.File(selectedImage!.path).readAsBytes();
     String filePath = selectedImage!.path;
     String? fileType = lookupMimeType(filePath);
 
@@ -22,6 +24,9 @@ class ProfileServiceImpl implements ProfileService {
     return done;
   }
 
+  ///Update profile name from [name]
+  ///
+  ///send [POST] response to backend to update user profile name
   @override
   Future<bool> updateProfileName(String name) async {
     bool done = (await httpProvider
@@ -32,6 +37,9 @@ class ProfileServiceImpl implements ProfileService {
     return done;
   }
 
+  ///Delete current user
+  ///
+  ///send [DELETE] response to backend to delete current user
   @override
   Future<bool> deleteCurrentUser() async {
     bool done = (await httpProvider.sendDelRequest(APIContract.userProfile));
