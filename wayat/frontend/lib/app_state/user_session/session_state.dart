3b51eec1107d50e3f2e8a23917d5bb2cd1ecb864
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-09-28 17:47:46 +0200

    Session state correct type in session_state doc

--- wayat/frontend/lib/app_state/user_session/session_state.dart
+++ wayat/frontend/lib/app_state/user_session/session_state.dart
@@ -7,22 +7,22 @@ import 'package:wayat/services/authentication/auth_service.dart';
 import 'package:wayat/services/authentication/gauth_service_impl.dart';
 part 'session_state.g.dart';
 
-/// Manages all the states for the athenticad user related
-/// functionality. 
-/// 
-/// Also, it wraps the authentication service. 
+/// Manages all the states for the authenticated user related
+/// functionality.
+///
+/// Also, it wraps the authentication service.
 // ignore: library_private_types_in_public_api
 class SessionState = _SessionState with _$SessionState;
 
 abstract class _SessionState with Store {
   /// Whether or not the authentication process has been completed.
-  /// 
+  ///
   /// Useful for **auto_route**.
   @observable
   bool finishLoggedIn = false;
 
   /// Whether or not the user has logged in with google.
-  /// 
+  ///
   /// Useful for **auto_route**.
   @observable
   bool googleSignedIn = false;
@@ -34,24 +34,24 @@ abstract class _SessionState with Store {
   bool hasDoneOnboarding = false;
 
   /// Instance of the authenticated user in the app.
-  /// 
+  ///
   /// It will be `null` if there is no authenticated user.
   @observable
   MyUser? currentUser;
 
-  /// Authentication service, useful for signing in an user 
+  /// Authentication service, useful for signing in an user
   /// (this includes signing silently), signing out an user and
   /// fetching authenticated user data, setting authenticated user data.
   final AuthService authService;
 
   /// App state for the user session related functionality.
-  /// 
+  ///
   /// If no [authService] is provided, a [GoogleAuthService]
   /// will be instantiated.
   _SessionState({AuthService? authService})
       : authService = authService ?? GoogleAuthService();
 
-  /// Calls [setDoneOnBoarding] function and then it changes 
+  /// Calls [setDoneOnBoarding] function and then it changes
   /// the state of `hasDoneOnboarding` to `true`.
   @action
   Future<void> doneOnBoarding() async {
@@ -61,9 +61,9 @@ abstract class _SessionState with Store {
 
   /// Checks if login process can be done without a
   /// graphical interface.
-  /// 
+  ///
   /// If this functions ends and [currentUser] remains `null`
-  /// or [googleSignedIn] remains `false`, it means the user 
+  /// or [googleSignedIn] remains `false`, it means the user
   /// could not be authenticated silently.
   Future<void> isLogged() async {
     if (currentUser != null) return;
@@ -90,7 +90,7 @@ abstract class _SessionState with Store {
   }
 
   /// Fetches all authenticated user data.
-  /// 
+  ///
   /// Only if the user has not been initialized yet.
   @action
   Future initializeUser() async {
@@ -104,8 +104,8 @@ abstract class _SessionState with Store {
   }
 
   /// Updates the [phone] of the authenticated user.
-  /// 
-  /// Returns `true` if the `phone` has benn successfully 
+  ///
+  /// Returns `true` if the `phone` has benn successfully
   /// updated.
   @action
   Future<bool> updatePhone(String phone) async {
@@ -114,10 +114,10 @@ abstract class _SessionState with Store {
     return done;
   }
 
-  /// Updates the [onboardingCompleted] of the 
+  /// Updates the [onboardingCompleted] of the
   /// authenticated user to `true`.
-  /// 
-  /// Returns `true` if the [onboardingCompleted] has benn 
+  ///
+  /// Returns `true` if the [onboardingCompleted] has benn
   /// successfully updated.
   @action
   Future<bool> setDoneOnBoarding() async {
@@ -155,8 +155,8 @@ abstract class _SessionState with Store {
     }
   }
 
-  /// Log out process. This includes closing the map, 
-  /// undoing changes in the login state and calling the 
+  /// Log out process. This includes closing the map,
+  /// undoing changes in the login state and calling the
   /// [authService] [signOut].
   Future logOut() async {
     final MapState mapState = GetIt.I.get<MapState>();
@@ -169,13 +169,13 @@ abstract class _SessionState with Store {
     currentUser = null;
   }
 
-  /// Checks if the onboarding process has been 
+  /// Checks if the onboarding process has been
   /// completed.
   bool isOnboardingCompleted() {
     return currentUser!.onboardingCompleted;
   }
 
-  /// Checks if the authenticated user has a 
+  /// Checks if the authenticated user has a
   /// phone number.
   bool hasPhone() {
     return currentUser != null && currentUser!.phone != "";
