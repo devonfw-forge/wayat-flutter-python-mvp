51a6a6a6d108c532c3b7ce522d753ed809e80d42
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-28 10:22:58 +0200

    contact service pr

--- wayat/frontend/lib/services/contact/flutter_contacts_handler_libw.dart
+++ wayat/frontend/lib/services/contact/flutter_contacts_handler_libw.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_contacts/flutter_contacts.dart';
 
+/// Wrapper for the [FlutterContacts] library.
+///
+/// [FlutterContacts] allows us to read contacts from the user's phone.
+///
+/// This wrapper is necessary to be able to mock the libary's functionality for testing purposes.
 class FlutterContactsHandlerLibW {
   Future<bool> requestPermission() async {
     return await FlutterContacts.requestPermission(readonly: true);
--- wayat/frontend/lib/services/contact/import_phones_service_impl.dart
+++ wayat/frontend/lib/services/contact/import_phones_service_impl.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:wayat/services/contact/flutter_contacts_handler_libw.dart';
 
+/// Service to read the contacts stored in the device and import them to the app.
 class ContactsAddressServiceImpl {
+  /// Reads all contacts stored in the device and returns only their phone numbers.
+  ///
+  /// First, it needs to check and/or ask for permission to read the contacts and,
+  /// if allowed, obtains them and returns just the phones.
+  ///
+  /// The optional parameter of type [FlutterContactsHandlerLibW] is present for testing purposes.
   Future<List<String>> getAllPhones(
       {FlutterContactsHandlerLibW? handler}) async {
     FlutterContactsHandlerLibW contactsHandler =
