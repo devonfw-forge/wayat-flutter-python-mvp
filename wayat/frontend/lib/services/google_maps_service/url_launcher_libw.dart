2e4fae7b9dee51b3d2c9ede96b2c6f265fcfa9d4
airherna <airam.hernandez-rocha@capgemini.com>
2022-09-28 12:11:15 +0200

    Update url_launcher_libw.dart

--- wayat/frontend/lib/services/google_maps_service/url_launcher_libw.dart
+++ wayat/frontend/lib/services/google_maps_service/url_launcher_libw.dart
@@ -1,9 +1,11 @@
+import 'package:url_launcher/url_launcher.dart' as launcher;
+
 class UrlLauncherLibW {
   Future<bool> canLaunchUrl(Uri url) async {
-    return await canLaunchUrl(url);
+    return await launcher.canLaunchUrl(url);
   }
 
   Future<bool> launchUrl(Uri url) async {
-    return await launchUrl(url);
+    return await launcher.launchUrl(url);
   }
 }
