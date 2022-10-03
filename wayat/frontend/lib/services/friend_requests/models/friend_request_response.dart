2650fa1debf157b5fdd07bdfa52bdcbc1746fa71
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-10-03 14:39:20 +0200

    Update comments

--- wayat/frontend/lib/services/friend_requests/models/friend_request_response.dart
+++ wayat/frontend/lib/services/friend_requests/models/friend_request_response.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
-
 import 'package:wayat/domain/contact/contact.dart';
 
+/// Contains list of sent and received Friends request
 class FriendRequestRespone {
+  /// List of contacts that user has sent friend request
   List<Contact> sentRequests;
+
+  /// List of contacts that have sent friend request to the user
   List<Contact> receivedRequests;
   FriendRequestRespone({
     required this.sentRequests,
