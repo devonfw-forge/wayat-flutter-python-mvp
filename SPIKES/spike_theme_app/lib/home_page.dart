7127a9760a31a2996d2d2e08a29ca424ada7c66d
Serhii Bets <serhii.bets@capgemini.com>
2022-10-04 10:58:51 +0200

    Add SPIKE to change theme of the app

---
+++ SPIKES/spike_theme_app/lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:spike_theme_app/primarycolor_switcher.dart';
+import 'package:spike_theme_app/theme_switcher.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static const double _containerWidth = 450.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme & Primary Color Switcher'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 17),
+          width: _containerWidth,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Theme'),
+              ),
+              ThemeSwitcher(),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Primary Color'),
+              ),
+              PrimaryColorSwitcher(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- SPIKES/spike_theme_app/lib/main.dart
+++ SPIKES/spike_theme_app/lib/main.dart
@@ -1,66 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:spike_theme_app/theme/light/light_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:spike_theme_app/app_colors.dart';
+import 'package:spike_theme_app/home_page.dart';
+import 'package:spike_theme_app/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: CustomAppTheme.lightTheme,
-      darkTheme: CustomAppTheme.darkTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(),
         ),
+      ],
+      child: Consumer<ThemeProvider>(
+        child: const HomePage(),
+        builder: (c, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.selectedThemeMode,
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: AppColors.getMaterialColorFromColor(
+                  themeProvider.selectedPrimaryColor),
+              primaryColor: themeProvider.selectedPrimaryColor,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: AppColors.getMaterialColorFromColor(
+                  themeProvider.selectedPrimaryColor),
+              primaryColor: themeProvider.selectedPrimaryColor,
+            ),
+            home: child,
+          );
+        },
       ),
-      floatingActionButton: ElevatedButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
