f2a03ee2699b758c3c40d8e01d74207001a657be
Serhii Bets <serhii.bets@capgemini.com>
2022-10-06 17:23:40 +0200

    commit

--- SPIKES/push_notifications/firebase_notifications/lib/main.dart
+++ SPIKES/push_notifications/firebase_notifications/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:notify/widgets/custom_home_page.dart';
--- SPIKES/push_notifications/firebase_notifications/lib/widgets/custom_home_page.dart
+++ SPIKES/push_notifications/firebase_notifications/lib/widgets/custom_home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +22,14 @@ class _HomePageState extends State<HomePage> {
   void registerNotification() async {
     await Firebase.initializeApp();
     _messaging = FirebaseMessaging.instance;
+
+    _messaging.getToken().then((token) {
+      print(token);
+    });
+
+    _messaging.getAPNSToken().then((token) {
+      print(token);
+    });
 
     NotificationSettings settings = await _messaging.requestPermission(
       alert: true,
