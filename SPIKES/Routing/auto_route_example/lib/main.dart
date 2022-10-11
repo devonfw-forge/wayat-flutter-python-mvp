f0bd004ee5393b06ea54e0df0d54f7579c82992e
airherna <airam.hernandez-rocha@capgemini.com>
2022-10-11 16:16:56 +0200

    updated spike to use mobx and getit

--- SPIKES/Routing/auto_route_example/lib/main.dart
+++ SPIKES/Routing/auto_route_example/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route_example/navigation/app_router.dart';
+import 'package:auto_route_example/state/app_state.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  GetIt.I.registerLazySingleton<AppState>(() => AppState());
   runApp(App());
 }
 
--- SPIKES/Routing/auto_route_example/lib/navigation/app_router.gr.dart
+++ SPIKES/Routing/auto_route_example/lib/navigation/app_router.gr.dart
@@ -17,56 +17,135 @@ class _$AppRouter extends RootStackRouter {
 
   @override
   final Map<String, PageFactory> pagesMap = {
+    HomeWrapperRoute.name: (routeData) {
+      final args = routeData.argsAs<HomeWrapperRouteArgs>(
+          orElse: () => const HomeWrapperRouteArgs());
+      return MaterialPageX<dynamic>(
+        routeData: routeData,
+        child: HomeWrapperPage(key: args.key),
+      );
+    },
     HomeRoute.name: (routeData) {
       return MaterialPageX<dynamic>(
-          routeData: routeData, child: const HomePage());
+        routeData: routeData,
+        child: const HomePage(),
+      );
     },
     ItemListRoute.name: (routeData) {
       final args = routeData.argsAs<ItemListRouteArgs>(
           orElse: () => const ItemListRouteArgs());
       return MaterialPageX<dynamic>(
-          routeData: routeData, child: ItemListPage(key: args.key));
+        routeData: routeData,
+        child: ItemListPage(key: args.key),
+      );
     },
     ItemRoute.name: (routeData) {
-      final args = routeData.argsAs<ItemRouteArgs>();
+      final args =
+          routeData.argsAs<ItemRouteArgs>(orElse: () => const ItemRouteArgs());
       return MaterialPageX<dynamic>(
-          routeData: routeData,
-          child: ItemPage(key: args.key, product: args.product));
+        routeData: routeData,
+        child: ItemPage(key: args.key),
+      );
     },
     NavigateToListRoute.name: (routeData) {
       return MaterialPageX<dynamic>(
-          routeData: routeData, child: const NavigateToListPage());
+        routeData: routeData,
+        child: const NavigateToListPage(),
+      );
     },
     BottomMenuRoute.name: (routeData) {
       return MaterialPageX<dynamic>(
-          routeData: routeData, child: const BottomMenuPage());
+        routeData: routeData,
+        child: const BottomMenuPage(),
+      );
     },
     BottomMenuSecondRoute.name: (routeData) {
       return MaterialPageX<dynamic>(
-          routeData: routeData, child: const BottomMenuSecondPage());
-    }
+        routeData: routeData,
+        child: const BottomMenuSecondPage(),
+      );
+    },
   };
 
   @override
   List<RouteConfig> get routes => [
-        RouteConfig(HomeRoute.name, path: '/', children: [
-          RouteConfig(NavigateToListRoute.name,
-              path: 'navigate-to-list-page', parent: HomeRoute.name),
-          RouteConfig(BottomMenuRoute.name,
-              path: 'bottom-menu-page', parent: HomeRoute.name),
-          RouteConfig(BottomMenuSecondRoute.name,
-              path: 'bottom-menu-second-page', parent: HomeRoute.name)
-        ]),
-        RouteConfig(ItemListRoute.name, path: '/item-list-page'),
-        RouteConfig(ItemRoute.name, path: '/item-page')
+        RouteConfig(
+          HomeWrapperRoute.name,
+          path: '/',
+          children: [
+            RouteConfig(
+              HomeRoute.name,
+              path: 'home-page',
+              parent: HomeWrapperRoute.name,
+              children: [
+                RouteConfig(
+                  NavigateToListRoute.name,
+                  path: 'navigate-to-list-page',
+                  parent: HomeRoute.name,
+                ),
+                RouteConfig(
+                  BottomMenuRoute.name,
+                  path: 'bottom-menu-page',
+                  parent: HomeRoute.name,
+                ),
+                RouteConfig(
+                  BottomMenuSecondRoute.name,
+                  path: 'bottom-menu-second-page',
+                  parent: HomeRoute.name,
+                ),
+              ],
+            ),
+            RouteConfig(
+              ItemListRoute.name,
+              path: 'item-list-page',
+              parent: HomeWrapperRoute.name,
+            ),
+            RouteConfig(
+              ItemRoute.name,
+              path: 'item-page',
+              parent: HomeWrapperRoute.name,
+            ),
+          ],
+        )
       ];
+}
+
+/// generated route for
+/// [HomeWrapperPage]
+class HomeWrapperRoute extends PageRouteInfo<HomeWrapperRouteArgs> {
+  HomeWrapperRoute({
+    Key? key,
+    List<PageRouteInfo>? children,
+  }) : super(
+          HomeWrapperRoute.name,
+          path: '/',
+          args: HomeWrapperRouteArgs(key: key),
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeWrapperRoute';
+}
+
+class HomeWrapperRouteArgs {
+  const HomeWrapperRouteArgs({this.key});
+
+  final Key? key;
+
+  @override
+  String toString() {
+    return 'HomeWrapperRouteArgs{key: $key}';
+  }
 }
 
 /// generated route for
 /// [HomePage]
 class HomeRoute extends PageRouteInfo<void> {
   const HomeRoute({List<PageRouteInfo>? children})
-      : super(HomeRoute.name, path: '/', initialChildren: children);
+      : super(
+          HomeRoute.name,
+          path: 'home-page',
+          initialChildren: children,
+        );
 
   static const String name = 'HomeRoute';
 }
@@ -75,8 +154,11 @@ class HomeRoute extends PageRouteInfo<void> {
 /// [ItemListPage]
 class ItemListRoute extends PageRouteInfo<ItemListRouteArgs> {
   ItemListRoute({Key? key})
-      : super(ItemListRoute.name,
-            path: '/item-list-page', args: ItemListRouteArgs(key: key));
+      : super(
+          ItemListRoute.name,
+          path: 'item-list-page',
+          args: ItemListRouteArgs(key: key),
+        );
 
   static const String name = 'ItemListRoute';
 }
@@ -95,24 +177,24 @@ class ItemListRouteArgs {
 /// generated route for
 /// [ItemPage]
 class ItemRoute extends PageRouteInfo<ItemRouteArgs> {
-  ItemRoute({Key? key, required Product product})
-      : super(ItemRoute.name,
-            path: '/item-page',
-            args: ItemRouteArgs(key: key, product: product));
+  ItemRoute({Key? key})
+      : super(
+          ItemRoute.name,
+          path: 'item-page',
+          args: ItemRouteArgs(key: key),
+        );
 
   static const String name = 'ItemRoute';
 }
 
 class ItemRouteArgs {
-  const ItemRouteArgs({this.key, required this.product});
+  const ItemRouteArgs({this.key});
 
   final Key? key;
 
-  final Product product;
-
   @override
   String toString() {
-    return 'ItemRouteArgs{key: $key, product: $product}';
+    return 'ItemRouteArgs{key: $key}';
   }
 }
 
@@ -120,7 +202,10 @@ class ItemRouteArgs {
 /// [NavigateToListPage]
 class NavigateToListRoute extends PageRouteInfo<void> {
   const NavigateToListRoute()
-      : super(NavigateToListRoute.name, path: 'navigate-to-list-page');
+      : super(
+          NavigateToListRoute.name,
+          path: 'navigate-to-list-page',
+        );
 
   static const String name = 'NavigateToListRoute';
 }
@@ -129,7 +214,10 @@ class NavigateToListRoute extends PageRouteInfo<void> {
 /// [BottomMenuPage]
 class BottomMenuRoute extends PageRouteInfo<void> {
   const BottomMenuRoute()
-      : super(BottomMenuRoute.name, path: 'bottom-menu-page');
+      : super(
+          BottomMenuRoute.name,
+          path: 'bottom-menu-page',
+        );
 
   static const String name = 'BottomMenuRoute';
 }
@@ -138,7 +226,10 @@ class BottomMenuRoute extends PageRouteInfo<void> {
 /// [BottomMenuSecondPage]
 class BottomMenuSecondRoute extends PageRouteInfo<void> {
   const BottomMenuSecondRoute()
-      : super(BottomMenuSecondRoute.name, path: 'bottom-menu-second-page');
+      : super(
+          BottomMenuSecondRoute.name,
+          path: 'bottom-menu-second-page',
+        );
 
   static const String name = 'BottomMenuSecondRoute';
 }
