0c363b8769433dd19711b513372e8d49b35a3acc
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-10-14 11:02:03 +0200

    Shell route wrapper added

--- SPIKES/Routing/go_router_example/lib/navigation/app_router.dart
+++ SPIKES/Routing/go_router_example/lib/navigation/app_router.dart
@@ -7,47 +7,58 @@ final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
 final GlobalKey<NavigatorState> _shellNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'shell');
+final GlobalKey<NavigatorState> _shellNavigatorKey2 =
+    GlobalKey<NavigatorState>(debugLabel: 'shell2');
 
 class AppRouter {
   static final router = GoRouter(
-    // redirect: ((context, state) => "/first"),
-    initialLocation: "/first",
-    navigatorKey: _rootNavigatorKey,
-    routes: [
-      ShellRoute(
+      initialLocation: "/first",
+      navigatorKey: _rootNavigatorKey,
+      debugLogDiagnostics: true,
+      routes: [
+        // Currently, a extra shell router is needed to put another screen ontop of a previous one, as happens with /first/item/list and /first/item
+        ShellRoute(
+          builder: (context, state, child) => Expanded(
+            child: child,
+          ),
           navigatorKey: _shellNavigatorKey,
-          builder: (context, state, child) {
-            return HomePage(child: child);
-          },
           routes: [
-            GoRoute(
-                path: "/first",
-                pageBuilder: (context, state) =>
-                    const NoTransitionPage(child: NavigateToListPage()),
+            ShellRoute(
+                navigatorKey: _shellNavigatorKey2,
+                builder: (context, state, child) {
+                  return HomePage(child: child);
+                },
                 routes: [
                   GoRoute(
-                      path: "itemslist",
-                      builder: (context, state) => ItemListPage(),
+                      path: "/first",
+                      pageBuilder: (context, state) =>
+                          const NoTransitionPage(child: NavigateToListPage()),
                       routes: [
                         GoRoute(
-                          parentNavigatorKey: _rootNavigatorKey,
-                          path: "item",
-                          builder: (context, state) =>
-                              ItemPage(product: state.extra as Product),
-                        )
-                      ])
-                ]),
-            GoRoute(
-              path: "/second",
-              pageBuilder: (context, state) =>
-                  const NoTransitionPage(child: BottomMenuPage()),
-            ),
-            GoRoute(
-              path: "/third",
-              pageBuilder: (context, state) =>
-                  const NoTransitionPage(child: BottomMenuSecondPage()),
-            ),
-          ])
-    ],
-  );
+                            parentNavigatorKey: _shellNavigatorKey,
+                            path: "itemslist",
+                            builder: (context, state) => ItemListPage(),
+                            routes: [
+                              GoRoute(
+                                parentNavigatorKey: _rootNavigatorKey,
+                                path: "item",
+                                builder: (context, state) =>
+                                    ItemPage(product: state.extra as Product),
+                              )
+                            ])
+                      ]),
+                  GoRoute(
+                    path: "/second",
+                    pageBuilder: (context, state) =>
+                        const NoTransitionPage(child: BottomMenuPage()),
+                  ),
+                  GoRoute(
+                    path: "/third",
+                    pageBuilder: (context, state) =>
+                        const NoTransitionPage(child: BottomMenuSecondPage()),
+                  ),
+                ])
+          ],
+        )
+      ]);
 }
--- SPIKES/Routing/go_router_example/lib/pages/bottom_menu_second_page.dart
+++ SPIKES/Routing/go_router_example/lib/pages/bottom_menu_second_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router_example/state/app_state.dart';
 
 class BottomMenuSecondPage extends StatelessWidget {
   const BottomMenuSecondPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Second bottom item"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Second bottom item"),
+        ElevatedButton(
+            onPressed: () => context.go("/first/itemslist/item",
+                extra: GetIt.I.get<AppState>().allProducts[0]),
+            child: const Text("Tomate frito"))
+      ],
     );
   }
 }
