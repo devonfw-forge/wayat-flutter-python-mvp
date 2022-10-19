a87301c5042ddd85555eab6b593af09090f8745d
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2022-10-19 16:50:20 +0200

    Progress page test passed

--- wayat/frontend/test/features/onboarding/pages/progress_page_test.dart
+++ wayat/frontend/test/features/onboarding/pages/progress_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:wayat/app_state/user_state/user_state.dart';
@@ -28,16 +29,22 @@ void main() {
   });
 
   Widget createApp(Widget body) {
-    return MaterialApp(
+    final router = GoRouter(initialLocation: "/", routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => Scaffold(
+          body: body,
+        ),
+      ),
+    ]);
+    return MaterialApp.router(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       onGenerateTitle: (context) {
         GetIt.I.get<LangSingleton>().initialize(context);
         return GetIt.I.get<LangSingleton>().appLocalizations.appTitle;
       },
-      home: Scaffold(
-        body: body,
-      ),
+      routerConfig: router,
     );
   }
 
