6144e90df5de0fa40632267c74058331ff9aeaa5
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-11-22 10:34:47 +0100

    WIP firedart

---
+++ wayat/frontend/lib/services/location_listener/firedart/firedart_listener_service.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:wayat/domain/location/contact_location.dart';
+import 'package:wayat/services/contact/contact_service.dart';
+import 'package:wayat/services/location_listener/firestore_model/firestore_data_model.dart';
+
+abstract class FiredartListenerService {
+
+  ///SetUp listener of contactLocation mode update from status
+  Future<void> setUpListener(
+      {required Function(List<ContactLocation>) onContactsRefUpdate,
+      required Function(bool) onLocationModeUpdate});
+
+  Future<void> onStatusUpdate(DocumentSnapshot<Map<String, dynamic>> event,
+      {required Function(List<ContactLocation>) onContactsRefUpdate,
+      required Function(bool) onLocationModeUpdate,
+      ContactService? contactService});
+
+  /// Closes current listener to Firestore
+  void cancelListenerSubscription();
+
+  /// Return active or passive location mode from Firestore status
+  @visibleForTesting
+  bool getLocationModeFromStatus(FirestoreDataModel firestoreData);
+
+  ///Return [ContactLocation]
+  ///
+  ///Get contact uid, location, address and last updated data
+  @visibleForTesting
+  Future<List<ContactLocation>> getContactRefsFromStatus(
+      FirestoreDataModel firestoreData,
+      {ContactService? contactService});
+}
