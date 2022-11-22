b62a11e66f69d1be4c6a303b9006f93a01c829a8
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2022-11-22 13:36:39 +0100

    Script refactor and support for linux

--- wayat/frontend/launcher.sh
+++ wayat/frontend/launcher.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-FLAGS=$(getopt -a --options hm:p: --long "help,mode:,platform:" -- "$@")
+FLAGS=$(getopt -a --options hp: --long "help,platform:" -- "$@")
 
 eval set -- "$FLAGS"
 while true; do
     case "$1" in
-        -m | --mode)              mode=$2; shift 2;;
         -p | --platform)          platform=$2; shift 2;;
         -h | --help)              help="true"; shift 1;;
         --) shift; break;;
@@ -19,7 +18,7 @@ green='\e[1;32m'
 red='\e[0;31m'
 
 # Common var
-currentPath=`pwd`
+currentPath=$(pwd)
 flutterDockerImage="../../cloud-builders/flutter"
 
 function help {
@@ -27,7 +26,6 @@ function help {
     echo "Generates local build for Android, Web and Windows. Also can be used to run local web version in release mode."
     echo ""
     echo "Common flags:"
-    echo "  -m, --mode                  [Required] Mode for the script, accepted values: 'build' or 'run'"
     echo "  -p, --platform              [Required] Platform name to build or run flutter project."
 
     exit
@@ -57,17 +55,32 @@ function checkGoogleServices {
     fi
 }
 
-function buildRelease {
-    case $platform in
-        web | android) echo -e "${green}Using platform ${platform} for ${mode}." ;;
-        *) echo -e "${red}Error: Platform ${platfrom} not supported for ${mode} is not supported." >&2; echo -ne ${white}; exit 1
-    esac
-    echo -e "${green}Building app..."; echo -ne ${white}
+function checkFlutterImage {
     # Check local flutter image with 3.3.6 version
     if [[ "$(docker images -q flutter_local:3.3.6 2> /dev/null)" == "" ]]; then
         if [ ! -f "${flutterDockerImage}/Dockerfile" ]; then echo -e "${red}Error: Docker image not found in ${flutterDockerImage}." >&2; echo -ne ${white}; exit 1; fi
         docker build ${flutterDockerImage} -t flutter_local:3.3.6 --build-arg FLUTTER_VERSION=3.3.6
+    else
+        echo -e "${green}Flutter image 3.3.6 already in local..."; echo -ne ${white}
     fi
+}
+
+function executeTesting {
+    echo -e "${green}Testing app..."; echo -ne ${white}
+
+    # Going to wayat dir to use frontend child dir as a volume
+    cd ..
+    cp frontend/launcher/test.yml .
+    docker-compose -f test.yml up || ( rm ./test.yml && exit 1 )
+    rm ./test.yml
+    cd $currentPath
+    
+    # Going to wayat dir to use frontend child dir as a volume
+    echo -e "${green}Test for ${platform} done."; echo -ne ${white}
+}
+
+function buildRelease {
+    echo -e "${green}Building app..."; echo -ne ${white}
     # Going to wayat dir to use frontend child dir as a volume
     cd ..
     case $platform in
@@ -84,48 +97,44 @@ function buildRelease {
             rm ./build-web.yml
             echo -e "${green}LOCATION: build/web"; echo -ne ${white}
             ;;
+        linux) 
+            cp frontend/launcher/build-linux.yml .
+            docker-compose -f build-linux.yml up || ( rm ./build-linux.yml && exit 1 )
+
+            rm ./build-linux.yml
+            echo -e "${green}LOCATION: build/linux"; echo -ne ${white}
+            ;;
     esac
     cd $currentPath
     echo -e "${green}Build for ${platform} done."; echo -ne ${white}
 }
 
-function runRelease {
-    case $platform in
-        web) echo -e "${green}Using platform ${platform} for ${mode}."; echo -ne ${white} ;;
-        *) echo -e "${red}Error: Platform ${platfrom} not supported for ${mode} is not supported." >&2; echo -ne ${white}; exit 1
-    esac
-    # Going to wayat dir to use frontend child dir as a volume
-    cd ..
-    cp frontend/launcher/build-web.yml .
-    docker-compose -f build-web.yml up || ( rm ./build-web.yml && exit 1 )
-
-    rm ./build-web.yml
+function runWebRelease {
     cd $currentPath
-    echo -e "${green}Build for ${platform} done."; echo -ne ${white}
     echo -e "${green}Running app..."; echo -ne ${white}
     docker-compose up
 }
 
+#==========================================
+# Script Execution
 
 if [[ "$help" == "true" ]]; then help; fi
 
-
-if [[ "$mode" == "" ]]; then echo -e "${red}Error: Mode not provided." >&2; echo -ne ${white}; exit 1; fi
 if [[ "$platform" == "" ]]; then echo -e "${red}Error: Platform not provided." >&2; echo -ne ${white}; exit 1; fi
 
-
 case $platform in
-    web | android | desktop) echo -e "${green}Using platform ${platform} for ${mode}."; echo -ne ${white} ;;
-    *) echo -e "${red}Error: Platform ${platfrom} not supported for ${mode} is not supported." >&2; echo -ne ${white}; exit 1
+    web | android | linux ) echo -e "${green}Using platform ${platform}."; echo -ne ${white} ;;
+    * ) echo -e "${red}Error: Platform ${platfrom} not supported is not supported." >&2; echo -ne ${white}; exit 1
 esac
 
 checkEnvFile
-checkGoogleServices
 
-if [[ "$platform" == "android" ]]; then checkKeystoreFile; checkPropertiesFile; fi
+if [[ "$platform" == "android" ]]; then checkKeystoreFile; checkPropertiesFile; checkGoogleServices; fi
 
-case $mode in
-    build) buildRelease ;;
-    run) runRelease ;;
-    *) echo -e "${red}Error: Mode ${mode} is not supported." >&2; echo -ne ${white}; exit 1
-esac
+checkFlutterImage
+
+executeTesting
+
+buildRelease
+
+if [[ "$platform" == "web" ]]; then runRelease; fi
