b248a8d7b637b06c4108fda30f3fb1641a480bd9
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-11-28 16:46:03 +0100

    WIP tests

---
+++ wayat/frontend/test/services/location_listener/rest_listener_service_test.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wayat/app_state/user_state/user_state.dart';
+import 'package:wayat/domain/contact/contact.dart';
+import 'package:wayat/services/common/http_provider/http_provider.dart';
+import 'package:wayat/services/contact/contact_service.dart';
+import 'package:wayat/services/location_listener/firestore_model/contact_ref_model.dart';
+import 'package:wayat/services/location_listener/rest_listener_service_impl.dart';
+
+import 'rest_listener_service_test.mocks.dart';
+
+@GenerateNiceMocks([
+  MockSpec<UserState>(),
+  MockSpec<HttpProvider>(),
+  MockSpec<ContactService>()
+  ])
+void main() async {
+  MockUserState mockUserState = MockUserState();
+  MockHttpProvider mockHttpProvider = MockHttpProvider();
+  MockContactService mockContactService = MockContactService();
+
+  Contact contact = Contact(
+        shareLocationTo: true,
+        id: "id",
+        name: "name",
+        email: "email",
+        imageUrl: "url",
+        phone: "phone");
+
+  when(mockContactService.getAll()).thenAnswer((_) => Future.value([contact]));
+
+  setUpAll(() {
+
+    GetIt.I.registerSingleton<UserState>(mockUserState);
+    GetIt.I.registerSingleton<HttpProvider>(mockHttpProvider);
+  });
+
+  test('Cancel all subscriptions', () {
+    FiredartListenerServiceImpl firedartListenerServiceImpl =
+        FiredartListenerServiceImpl();
+
+    firedartListenerServiceImpl.cancelListenerSubscription();
+  });
+
+  test('contactRefsToContactLocations ', () {
+    FiredartListenerServiceImpl firedartListenerServiceImpl =
+        FiredartListenerServiceImpl();
+
+    ContactRefModel contactRefModel = ContactRefModel(
+      address: '',
+      lastUpdated: Timestamp.now(),
+      location: const GeoPoint(10.2, 20.3),
+      uid: 'id'
+    );
+
+    firedartListenerServiceImpl.contactRefsToContactLocations([contactRefModel], contactService: mockContactService);
+  });
+  
+}
